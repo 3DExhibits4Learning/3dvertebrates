0c4a609e9b9f488314f03640aaab9888be52644d
AJ <ab632@humboldt.edu>
2025-04-17 09:01:28 -0700

    commit before mac merge

--- src/components/Collections/SketchFabAPI.tsx
+++ src/components/Collections/SketchFabAPI.tsx
@@ -21,6 +21,7 @@ import AnnotationModal from '@/components/Collections/AnnotationModal'
 import Sketchfab from '@sketchfab/viewer-api'
 import ModelAnnotation from './AnnotationModel'
 import Vertebrates from '@/classes/HerbariumClass'
+import { stripTagsExceptIAP } from '@/functions/client/utils/utils'
 
 const SFAPI = (props: { gMatch: { hasInfo: boolean; data?: GbifResponse }, model: model, images: GbifImageResponse[], imageTitle: string }) => {
 
@@ -268,7 +269,7 @@ const SFAPI = (props: { gMatch: { hasInfo: boolean; data?: GbifResponse }, model
                   </div>
                   <div id="annotationDivText">
                     <br></br>
-                    <p dangerouslySetInnerHTML={{ __html: (annotations[index - 1].annotation as photo_annotation).annotation }} className='m-auto pr-[3%] pl-[2%] text-center fade' />
+                    <p dangerouslySetInnerHTML={{ __html: stripTagsExceptIAP((annotations[index - 1].annotation as photo_annotation).annotation) }} className='m-auto pr-[3%] pl-[2%] text-center fade' />
                   </div>
                   <div id="annotationDivCitation">
                     <br></br>
@@ -293,7 +294,7 @@ const SFAPI = (props: { gMatch: { hasInfo: boolean; data?: GbifResponse }, model
                   </div>
                   <div id="annotationDivText">
                     <br></br>
-                    <p dangerouslySetInnerHTML={{ __html: (annotations[index - 1].annotation as model_annotation).annotation }} className='m-auto pr-[3%] pl-[2%] text-center fade' />
+                    <p dangerouslySetInnerHTML={{ __html: stripTagsExceptIAP((annotations[index - 1].annotation as model_annotation).annotation) }} className='m-auto pr-[3%] pl-[2%] text-center fade' />
                   </div>
                   <div id="annotationDivCitation">
                     <br></br>
