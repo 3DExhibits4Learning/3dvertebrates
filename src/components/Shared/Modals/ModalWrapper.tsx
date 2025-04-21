2b6d4f539319ebbbc01c263b8a0df16c5350048a
AJ <ab632@humboldt.edu>
2025-04-21 14:10:20 -0700

    hh wip

--- src/components/Shared/Modals/ModalWrapper.tsx
+++ src/components/Shared/Modals/ModalWrapper.tsx
@@ -4,8 +4,8 @@
  * @fileoverview allows annotators to reorder the annotation numbers
  */
 
-import { Modal, ModalContent } from "@nextui-org/react"
-import { Dispatch, SetStateAction, useContext, useState, useEffect } from "react"
+import { Button, Modal, ModalContent } from "@nextui-org/react"
+import { Dispatch, SetStateAction, useContext, useState, useEffect, ChangeEvent } from "react"
 import { AnnotationClientData } from "@/components/Admin/Annotation/Annotation Client/AnnotationClient"
 import { annotationClientData, fullAnnotation } from "@/interface/interface"
 
@@ -16,15 +16,41 @@ export default function AnnotationReorder(props: { isOpen: boolean, setIsOpen: D
     const initialAnnotationNumbers: { id: string, no: string }[] = []
 
     const [annotationNumbers, setAnnotationNumbers] = useState<{ id: string, no: string }[]>()
+    const [saveDisabled, setSaveDisabled] = useState(false)
 
-    const AnnotationNumber = (props:{annotation: {id: string, no: string}}) => <div className="flex justify-center border p-4 w-24">
-        <input type='number' min='2' max={annotations.length + 1} value={props.annotation.no} className="h-12 w-16 text-xl text-center"></input>
-    </div>
+    const AnnotationNumber = (props: { annotation: { id: string, no: string } }) => {
+
+        const numberChangeHandler = (no: string) => {
+            const newAnnotationNumbers = annotationNumbers?.map(annotationNumber => annotationNumber.id === props.annotation.id ? { id: annotationNumber.id, no: no} : annotationNumber)
+            setAnnotationNumbers(newAnnotationNumbers)
+        }
+
+        return <div className="flex justify-center border p-4 w-24">
+            <input
+                onChange={e => numberChangeHandler(e.target.value)}
+                type='number'
+                min='2'
+                max={annotations.length + 1}
+                value={props.annotation.no}
+                className="h-12 w-16 text-xl text-center">
+            </input>
+        </div>
+    }
 
     useEffect(() => {
         annotations.every(annotation => initialAnnotationNumbers.push({ id: annotation.annotation_id, no: annotation.annotation_no.toString() }))
         setAnnotationNumbers(initialAnnotationNumbers as { id: string, no: string }[])
     }, [])
+
+    useEffect(() => {
+        if(annotationNumbers){
+            const numbers = annotationNumbers.map(annotationNumbers => parseInt(annotationNumbers.no))
+            const unique = [...new Set(numbers)]
+            setSaveDisabled(!(unique.length === annotationNumbers.length))
+        }
+    }, [annotationNumbers])
+
+    console.log('ModalWrapper.tsx')
 
     return <Modal isOpen={props.isOpen} size='xl' hideCloseButton>
         <ModalContent>
@@ -34,11 +60,13 @@ export default function AnnotationReorder(props: { isOpen: boolean, setIsOpen: D
                     {
                         annotationNumbers && annotations?.map((annotation, index) => <>
                             <div className="flex border text-2xl items-center pl-3" key={index}>{annotation.title}</div>
-                            <AnnotationNumber annotation={annotationNumbers.find(annotationNumber => annotation.annotation_id === annotationNumber.id) as { id: string, no: string }}/>
+                            <AnnotationNumber key={annotation.annotation_id} annotation={annotationNumbers.find(annotationNumber => annotation.annotation_id === annotationNumber.id) as { id: string, no: string }} />
                         </>)
                     }
                 </section>
             </article>
+
+            <div className="flex w-full justify-center p-8"><Button isDisabled={saveDisabled} className="w-4/5">Save Annotation Numbers</Button></div>
 
         </ModalContent>
     </Modal>
