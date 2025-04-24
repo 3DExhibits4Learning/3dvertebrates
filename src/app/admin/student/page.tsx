f8be3d75278d638b9b51d0efeb3ed14ac0aa0fea
AJ <ab632@humboldt.edu>
2025-04-24 13:56:15 -0700

    diddo

--- src/app/admin/student/page.tsx
+++ src/app/admin/student/page.tsx
@@ -52,7 +52,7 @@ export default async function Page() {
 
         // Filter assigned models
         const studentAssignmentUids = assignments.filter(assignment => assignment.email === email).map(assignment => assignment.uid)
-        const assignedModels = process.env.NODE_ENV === 'development' ?  modelsToAnnotate.filter(model => model.uid === 'ee451c036e3d45398f8a1f2ad78367c3') : modelsToAnnotate.filter(model => studentAssignmentUids.includes(model.uid))
+        const assignedModels = process.env.NODE_ENV === 'test' ?  modelsToAnnotate.filter(model => model.uid === 'ee451c036e3d45398f8a1f2ad78367c3') : modelsToAnnotate.filter(model => studentAssignmentUids.includes(model.uid))
 
         // Typical client
         return <>
