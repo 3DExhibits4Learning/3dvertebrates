64409f381b9cd76f8c316db034e50f73e7f1c732
AJ <ab632@humboldt.edu>
2025-05-07 10:29:30 -0700

    reverting abi's changes

--- src/app/admin/student/page.tsx
+++ src/app/admin/student/page.tsx
@@ -44,15 +44,13 @@ export default async function Page() {
         // Get models to annotate, annotation models (models used as annotations themselves), and assignments
         const modelsToAnnotate = await getModelsToAnnotate().catch(e => serverErrorHandler(path, e.message, "Couldn't get models to annotate", 'getModelsToAnnotate()', false)) as fullModel[]
         const annotationModels = await getAllAnnotationModels().catch(e => serverErrorHandler(path, e.message, "Couldn't get annotation models", 'getModelsToAnnotate()', false)) as model[]
-        const abiModels = annotationModels.filter(model => model.spec_name === 'Martes americana')
+        //const abiModels = annotationModels.filter(model => model.spec_name === 'Martes americana')
         const assignments = await getAssignments().catch(e => serverErrorHandler(path, e.message, "Couldn't get assignments", 'getAssignments()', false)) as assignment[]
 
         // Get modelAnnotations and filter for unused annotations
         const modelAnnotations = await getModelAnnotations().catch(e => serverErrorHandler(path, e.message, "Couldn't get assignments", 'getAssignments()', false)) as annotationWithModel[]
         const unusedModelAnnotations = annotationModels.filter(model => isAnnotationModel(model) && !isUsedAnnotationModel(model, modelAnnotations))
-        for (let i in abiModels) unusedModelAnnotations.push(abiModels[i])
-        console.log('Annotation Models: ', unusedModelAnnotations)
-        console.log(unusedModelAnnotations.length)
+        //for (let i in abiModels) unusedModelAnnotations.push(abiModels[i])
 
         // Filter assigned models
         const studentAssignmentUids = assignments.filter(assignment => assignment.email === email).map(assignment => assignment.uid)
