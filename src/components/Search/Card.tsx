c5f335ac82c35bdf5e2cd1e2bc76ee01c9876fd9
AJ <ab632@humboldt.edu>
2025-06-25 11:36:09 -0700

    Card aria labels

--- src/components/Search/Card.tsx
+++ src/components/Search/Card.tsx
@@ -14,12 +14,11 @@ import { Skeleton } from "@heroui/react"
 
 // Default imports
 import noImage from '../../../public/noImage.png'
+import Link from "next/link"
 
 // Main JSX
 export default function Card(props: { model: model }) {
     const model = props.model
-
-    const [mobileAnnotationModalOpen, setMobileAnnotationModalOpen] = useState(false)
     const [src, setSrc] = useState('')
     const thumbnailPath = model.thumbnail ? getNfsPath(model.thumbnail) : ''
 
@@ -48,24 +47,25 @@ export default function Card(props: { model: model }) {
             {
                 // Img displays once loaded
                 src && <section className='rounded shadow-md mx-auto'>
-                    <a href={model.base_model ? "/collections/" + model.spec_name : "/collections/" + model.spec_name + `?annotation=${model.uid}`} tabIndex={-1}>
+                    <Link href={model.base_model ? "/collections/" + model.spec_name : "/collections/" + model.spec_name + `?annotation=${model.uid}`} tabIndex={-1} aria-label={`Go to 3D Model of ${model.spec_name}`}>
                         <img
                             alt={'Image of ' + model.spec_name}
                             role='button'
                             src={src}
                             className='w-full h-[calc(100vh-275px)] min-h-[25rem] max-h-[30rem] object-cover relative z-5 rounded-t-md'
                             onError={(e: SyntheticEvent<HTMLImageElement, Event>) => { handleImgError(e.currentTarget, noImage) }} />
-                    </a>
+                    </Link>
                 </section>
             }
             <section className='bg-[#98B8AD] dark:bg-[#3d3d3d] h-[5rem] max-h-[calc(100vh-300px)*0.2] opacity-[0.99] px-5 py-3 rounded-b-md text-center relative z-10 flex flex-col justify-center items-center space-y-1.5 mt-[-1px]'>
                 <section className='flex items-center space-x-0.5rem'>
-                    <a
+                    <Link
+                    aria-label={`Go to 3D Model of ${model.pref_comm_name}`}
                         href={"/collections/" + (model).spec_name}
                         rel='noopener noreferrer'
                         className='text-[#004C46] dark:text-[#C3D5D1] text-xl'>
                         <i className='text-lg'>{model.spec_name.charAt(0).toUpperCase() + model.spec_name.slice(1)}</i>
-                    </a>
+                    </Link>
                 </section>
                 <section className='text-sm text-black dark:text-white'>
                     {toUpperFirstLetter(model.pref_comm_name)}
