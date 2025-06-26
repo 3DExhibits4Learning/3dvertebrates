296ea1d1a905e5e0028e651e985aec147fd66a74
AJ <ab632@humboldt.edu>
2025-06-26 13:46:47 -0700

    patched admin 'link'

--- src/components/Header/LogoAndSignIn.tsx
+++ src/components/Header/LogoAndSignIn.tsx
@@ -1,14 +1,9 @@
-// @ts-nocheck
 'use client'
 
-// Consider turning off ts-nocheck when coding/debugging.
-// The only line that expects an error is the conditional line in the DropdownMenu as its type doesn't allow for conditional rendering (or ts-ignore)
-
 // Typical imports
-import { signIn, signOut, useSession, } from "next-auth/react"
+import { signOut, useSession, } from "next-auth/react"
 import { useRouter } from "next/navigation"
-import { admin } from "@/functions/utils/devAuthed"
-import { NavbarContent, Button, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar } from "@heroui/react"
+import { NavbarContent, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar } from "@heroui/react"
 
 // Default imports
 import Image from "next/image"
@@ -27,8 +22,8 @@ export default function LogoAndSignIn() {
                 <DropdownTrigger>
                     <Avatar className="cursor-pointer" isFocusable={true} src={session?.user?.image!} name={session?.user?.name!} aria-label="Dropdown Menu Toggle" />
                 </DropdownTrigger>
-                <DropdownMenu aria-label="Static Actions">
-                    <DropdownItem key="Admin" ><Link aria-label="Go to Admin Page" href='/admin'>Admin</Link></DropdownItem>
+                <DropdownMenu aria-label="Dropdown menu">
+                    <DropdownItem key="Admin" onClick={() => router.push('/admin')} aria-label='Go to admin page'>Admin</DropdownItem>
                     <DropdownItem key="signOut"><button onClick={() => signOut()} aria-label='Sign Out'>Sign Out</button></DropdownItem>
                 </DropdownMenu>
             </Dropdown>
